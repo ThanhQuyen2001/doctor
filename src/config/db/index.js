7309b7420971600d3789644390cb34ea15750181
Thanh Quyen <quyen090401@gmail.com>
2023-06-28 15:49:30 +0700

    change db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect(
+        const uri =
             process.env.DATABASE_URL ||
-                'mongodb+srv://quyen090401:123456a%40@cluster0.atvuf3c.mongodb.net/doctor',
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true,
-            },
-        );
+            'mongodb+srv://quyen090401:123456a@@cluster0.atvuf3c.mongodb.net/doctor';
+        console.log(uri, 'uri');
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
         console.log('Connect successfully!!!');
     } catch (error) {
-        console.log('Connect failure!!!');
+        console.log('Connect failure!!!', error);
     }
 }
 
