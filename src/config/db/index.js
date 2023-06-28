be4850c83e7ac2a396365550cb5bec67949d134f
Thanh Quyen <quyen090401@gmail.com>
2023-06-28 14:29:41 +0700

    update mongo

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/doctor', {
+        await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
