e9e202936aaf8736f902ca01d25c76844316b6ce
Thanh Quyen <quyen090401@gmail.com>
2023-06-28 14:51:30 +0700

    update env

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override');
 const route = require('./routes');
 const db = require('./config/db');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 db.connect();
 app.use(
